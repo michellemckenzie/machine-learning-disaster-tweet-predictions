b8773696f9dfc38182190cdd15ec15c1a49ee6a2
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2022-05-20 18:23:49 -0700

    added more abbreviated_list

--- abbreviate.py
+++ abbreviate.py
@@ -3,7 +3,16 @@ read_file = pd.read_csv('emojitweets.csv')
 
 abbreviated_list = {
     "'til": "until",
-    "i'll": "i will",
+    "'ll": " will",
     "y'all": "you all",
     "that's": "that is",
+    "won't": "will not",
+    "can't": "can not",
+    "cannot": "can not",
+    "ain't": "am not",
+    "n't": " not", # wasn't weren't haven't didn't
+    "'ve": " have", # i've we've you've would've
+    "'d": " would", #you'd i'd we'd you'd
+    "'re": " are", # they're we're you're
+    "in'": "ing", # lookin'
 }
